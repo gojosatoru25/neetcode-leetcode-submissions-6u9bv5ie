bf4e5f293466a9985919e850f2f38cf0fe467f00
gojosatoru25 <154448325+gojosatoru25@users.noreply.github.com>
2026-07-19 15:59:25 +0530

    Time: 299 ms (78.27%) | Memory: 31.6 MB (91.78%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.py
@@ -14,17 +14,15 @@ class Solution:
     def minDays(self, bloomDay: List[int], m: int, k: int) -> int:
         n= len(bloomDay)
         total_flowers= m*k
-        ans= 0
         if total_flowers>n:
             return -1
         low,high= min(bloomDay),max(bloomDay)
         while low<=high:
             mid= low+(high-low)//2
             if self.ispossible(bloomDay,mid,m,k):
-                ans= mid
                 high=mid-1
             else:
                 low= mid+1
-        return ans 
+        return low 
                 
         
