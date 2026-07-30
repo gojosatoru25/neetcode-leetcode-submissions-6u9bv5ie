1c9b659a1c3cf6d2065c9ab9407e6b48ef384f4f
gojosatoru25 <154448325+gojosatoru25@users.noreply.github.com>
2026-07-30 21:15:43 +0530

    Time: 3 ms (70.84%) | Memory: 19.3 MB (82.05%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.py
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -17,9 +17,9 @@ class Solution:
             return -1
         while low<=high:
             mid= low+(high-low)//2
-            if self.max_sum(nums,mid,n)>k:
-                low= mid+1
-            else:
+            if self.max_sum(nums,mid,n)<=k:
                 high= mid-1
+            else:
+                low= mid+1
         return low
         
