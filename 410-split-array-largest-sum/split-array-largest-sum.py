68ea4984a3ce32b981b8754f4df6a88b2a3e76b7
gojosatoru25 <154448325+gojosatoru25@users.noreply.github.com>
2026-07-30 21:13:23 +0530

    Time: 4 ms (30.93%) | Memory: 19.3 MB (46.41%) - LeetSync

---
+++ 410-split-array-largest-sum/split-array-largest-sum.py
@@ -0,0 +1,25 @@
+class Solution:
+    def max_sum(self,nums,min_sum,n):
+        count_subarray=1
+        sums=0
+        for i in range(n):
+            if nums[i]+sums<=min_sum:
+                sums+=nums[i]
+            else:
+                count_subarray+=1
+                sums= nums[i]
+        return count_subarray
+    def splitArray(self, nums: List[int], k: int) -> int:
+        n= len(nums)
+        low= max(nums) 
+        high= sum(nums)
+        if k>n:
+            return -1
+        while low<=high:
+            mid= low+(high-low)//2
+            if self.max_sum(nums,mid,n)>k:
+                low= mid+1
+            else:
+                high= mid-1
+        return low
+        
